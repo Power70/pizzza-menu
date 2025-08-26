a9d2aac961933a858c7eb62fd0c23727eeb6f9b3
Power70 <nwisuanu@gmail.com>
2025-08-26 22:26:03 +0100

    element controller

--- src/App.js
+++ src/App.js
@@ -1,53 +1,132 @@
+// import { useState } from "react";
+
 import { useState } from "react";
 
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
+// const messages = [
+//   "Learn React ⚛️",
+//   "Apply for jobs 💼",
+//   "Invest your new income 🤑",
+// ];
+
+// export default function App () {
+//   const [step, setStep] = useState(1);
+//   const [isOpen, setIsOpen] = useState(true);
+
+//   function handleNext () {
+//     if (step < messages.length) {
+//       setStep((s) => s + 1);
+//     }
+//   }
+
+//   function handlePrevious () {
+//     if (step > 1) {
+//       setStep((s) => s - 1);
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
+
+//       {isOpen && <div className="steps">
+//       <div className="numbers">
+//         <div className={`step ${step === 1 ? "active" : ""}`}>1</div>
+//         <div className={`step ${step === 2 ? "active" : ""}`}>2</div>
+//         <div className={`step ${step === 3 ? "active" : ""}`}>3</div>
+//       </div>
+//       <p>
+//         step {step}: {messages[step - 1]}
+//       </p>
+
+//       <div className="buttons">
+//         <button style={{background: "#7950f2", color: "#fff"}} 
+//         onClick={handlePrevious
+//         }>
+//           Previous
+//         </button>
+//         <button style={{background: "#7950f2", color: "#fff"}} 
+//         onClick={handleNext}>
+//           Next
+//         </button>
+//       </div>
+//     </div>}
+//     </div>
+//   )
+// }
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
 
-export default function App () {
-  const [step, setStep] = useState(1);
-  const [isOpen, setIsOpen] = useState(true);
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
 
-  function handleNext () {
-    if (step < messages.length) {
-      setStep((s) => s + 1);
-    }
-  }
+function Logo () {
+  return <h1>Far Away 🏕️</h1>
+}
+function Form () {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-  function handlePrevious () {
-    if (step > 1) {
-      setStep((s) => s - 1);
-    }
+  const newItem = { description, quantity, packed: false, id: Date.now() };
+
+  function handleSubmit (event) {
+    event.preventDefault();
   }
 
   return (
-    <div>
-      <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
+    <form className="add-form" onSubmit={handleSubmit}>
+        <h3>What do you need for your 😍 trip?</h3>
+        <select 
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value)) }
+        >
+          {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option key={num} value={num}>{num}
+          </option>
+        ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button>Add</button>
+    </form>
+  );
+}
 
-      {isOpen && <div className="steps">
-      <div className="numbers">
-        <div className={`step ${step === 1 ? "active" : ""}`}>1</div>
-        <div className={`step ${step === 2 ? "active" : ""}`}>2</div>
-        <div className={`step ${step === 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p>
-        step {step}: {messages[step - 1]}
-      </p>
+function PackingList () {
+  return <div className="list">
+    <ul>
+      {initialItems.map(item => ( 
+        <Item item={item} />
+      ))}
+    </ul>
+  </div>;
+}
+  
+function Item ({item}) {
+  return <li>
+    <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+      {item.quantity} {item.description}
+    </span>
+    <button>❌</button>
+  </li>;
+}
 
-      <div className="buttons">
-        <button style={{background: "#7950f2", color: "#fff"}} 
-        onClick={handlePrevious
-        }>
-          Previous
-        </button>
-        <button style={{background: "#7950f2", color: "#fff"}} 
-        onClick={handleNext}>
-          Next
-        </button>
-      </div>
-    </div>}
-    </div>
-  )
+function Stats () {
+  return <footer className="stats">
+    <em>💼 You have X items on your list, and you already packed X (X%)</em>
+  </footer>
 }
